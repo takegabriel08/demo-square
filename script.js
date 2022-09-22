2cd74d98ced3d01082ee2c3afc8c8d691f5143e2
Gabriel <Gabriel@DESKTOP-7AUAOTD>
2022-09-22 21:43:04 +0300

    Added comments for chPos function

--- script.js
+++ script.js
@@ -22,24 +22,33 @@ input.addEventListener("keypress", function (event) {
   }
 });
 
+// Select button from dom
 var button = document.querySelector('input.chPos');
+// Select Square shape from dom
 var square = document.querySelector('div#square');
+// positions array to loop through
 const positions = ["top-left", "top-right", "bottom-right", "bottom-left"];
+// Declare index as var so it is accessible from inside event function
 var index;
+// set square initial opsition from js so that we don't mess values up with css values.
 square.style.bottom = "10px"
 square.style.left = "10px"
 square.style.top = ""
 square.style.right = ""
 
+// Function that has eventlistener
 button.addEventListener('click', (event) => {
+  // if index is equal to the last item in positions looped, then reset index
+  // if index is undefined set it as 0(almost not needed)
   if (index == positions.length - 1 || index == undefined) {
     index = 0
   } else {
     index = index + 1
   }
 
+ // log index for debugging.. =))
   console.log(index)
-
+// positions[index] is equal to positions[0 or 1 or 2 or 3] selecting from positions the required case for our situation
   if (positions[index] == "top-left") {
     square.style.bottom = ""
     square.style.left = "10px"
@@ -65,12 +74,11 @@ button.addEventListener('click', (event) => {
     square.style.top = ""
     square.style.right = "10px"
   }
+  // if no cases match (again almost impossible...) reset the position
   else {
     square.style.bottom = ""
     square.style.left = "10px"
     square.style.top = "10px"
     square.style.right = ""
   }
-
-
 });
